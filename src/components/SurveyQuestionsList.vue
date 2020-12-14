221d3562ab08f086ad22f45ce702637a686479ce
Roger Yu <roger@sproutsystems.com.au>
2020-12-14 17:15:47 +1100

    Made SurveyQuestion modular.

---
+++ src/components/SurveyQuestionsList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="survey-question-wrap">
+    
+    <h1>{{survey.name}}</h1>
+
+    <SurveyQuestionsListItem v-for="(question,index) in survey.questions" 
+    :key="question.id"
+    :question="question"
+    :index="index"
+     />
+
+    
+
+    <div class="back-button-wrap">
+      <router-link v-bind:to="'/Surveys'">
+        <button>Back</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import SurveyQuestionsListItem from './SurveyQuestionsListItem.vue';
+
+
+export default {
+  name: 'SurveyQuestionsList',
+  props: ['survey'],
+  components: {
+    SurveyQuestionsListItem,
+  },
+}
+</script>
+
+<style scoped>
+.survey-question-wrap {
+  width:90%;
+  padding-left:5%;
+}
+
+.back-button-wrap {
+  float:right;
+}
+</style>
---
+++ src/components/SurveyQuestionsListItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="survey-question-item">
+      <h3>Question {{index+1}}</h3>
+      <h5>{{question.title}}</h5>
+      <h6>{{question.subTitle}}</h6>
+
+      <div v-for="option in question.options" class="survey-question-option-item" 
+          v-bind:key="option.id">
+        <input type="checkbox" v-bind:key="option.id">
+        <label for="checkbox">{{ option.text }}</label>      
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SurveyQuestionsListItem',
+  props: ['question','index'],
+  
+}
+</script>
+
+<style scoped>
+
+.survey-question-item{
+    border-radius: 10px;
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+.survey-question-item h3,h5{
+  margin: -1px 0px 2px 0px;
+}
+.survey-question-item h6{
+  margin: -1px 0px 15px 0px;
+}
+
+</style>
